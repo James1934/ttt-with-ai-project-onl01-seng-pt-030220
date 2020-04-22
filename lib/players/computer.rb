bd34507fe42b270d3f321bf8087696b2cc18c71e
Bartholomew Fountain <baertholomew@gmail.com>
2020-04-22 14:23:53 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,7 +2,7 @@ module Players
   class Computer < Player
 
     def move(board)
-      if board.cells[4] == " "
+     # if board.cells[4] == " "
         "5"
       elsif board.cells[0] == " "
         "1"
